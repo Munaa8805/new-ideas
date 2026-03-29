4482daf275f43927312ed7239518a60e94bf31b8
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2026-03-29 16:22:24 -0600

    change route

--- src/App.jsx
+++ src/App.jsx
@@ -17,35 +17,19 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route
-            path="ideas/:ideaId/edit"
-            element={
-              <ProtectedRoute>
-                <EditIdea />
-              </ProtectedRoute>
-            }
-          />
           <Route path="ideas" element={<Ideas />} />
-          <Route
-            path="new-idea"
-            element={
-              <ProtectedRoute>
-                <NewIdea />
-              </ProtectedRoute>
-            }
-          />
           <Route path="projects" element={<Projects />} />
           <Route path="projects/:id" element={<ProjectDetails />} />
+          <Route path="" element={<ProtectedRoute />}>
+            <Route path="ideas/:ideaId/edit" element={<EditIdea />} />
+            <Route path="new-idea" element={<NewIdea />} />
+          </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route
-            path="profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
+          <Route path="" element={<ProtectedRoute />}>
+            <Route path="profile" element={<Profile />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
